e85b535ec5ce5ab7fddc416535b872a7f8cfe034
Sandro Costa <sandrocosta2003@gmail.com>
2025-07-28 13:39:05 +0200

    Fix bug

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -34,15 +34,20 @@ export const useProductStore = create((set) => ({
         return { success: true, message: data.message };
     },
     updateProduct: async (pid, updatedProduct) => {
-        price.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
-        const data = await res.json();
-        if (!data.success) return { success: false, message: data.message };
+        const res = await fetch(`/api/products/${pid}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedProduct),
+		});
+		const data = await res.json();
+		if (!data.success) return { success: false, message: data.message };
 
-        set(state => ({
-            products: state.products.map((product) =>
-                product._id === pid ? data.data : product
-            ),
-        }));
-        return { success: true, message: data.message };
+		set((state) => ({
+			products: state.products.map((product) => (product._id === pid ? data.data : product)),
+		}));
+
+		return { success: true, message: data.message };
     },
 }));
